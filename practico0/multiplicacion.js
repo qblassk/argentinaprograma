977f41b5b2b8ce06d1fb1345b7f9a7270b0ecdc1
Gustavo Bulaschevich <g.bulaschevich@gmail.com>
2023-05-25 12:39:36 -0300

    resta y multiplicacion

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,7 @@
+const numA = 2;
+const numB = 17;
+const numC = 6;
+
+const producto = numA * numB * numC;
+
+console.log(producto);
---
+++ practico0/resta.js
@@ -0,0 +1,6 @@
+const numA = 10;
+const numB = 2;
+
+const resto = numA - numB;
+
+console.log(resto);
